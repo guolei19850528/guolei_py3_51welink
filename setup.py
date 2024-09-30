21579cb49d180bbb8cfa8e5bc8702e6b674b1fac
guolei <174000902@qq.com>
2024-09-30 13:22:52 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="guolei-py3-51welink",
-    version="2.0.1",
+    version="2.0.2",
     description="微网通联 API",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,6 +19,7 @@ setup(
     packages=setuptools.find_packages('./'),
     install_requires=[
         "guolei-py3-requests",
+        "requests",
         "addict",
         "retrying",
         "jsonschema",
